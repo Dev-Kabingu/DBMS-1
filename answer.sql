3fa168020c3e05d6b8ef536bd78b7c5c0122b60e
dev-kabingu <missmercykabingu@gmail.com>
2024-11-21 13:11:27 +0300

    DBMS PLP Assignment 1

---
+++ answer.sql
@@ -0,0 +1,50 @@
+State and Explain the components of a DBMS(Database Management System)
+DBMS is a software that facilitates the storage, mangement, retrival of data in an organized manner.
+components: database schema which defines the organization of data.
+            query processor which intreprates and executes database queries.
+            database engine which allows the access and data management.
+            data management language that allow the manipulation of data.
+            security manager which controls users access to data.
+            backup and recovery manager which protects data by providing regular updates and recovery mechanisms
+            
+
+What is a relational database? Give 4 examples.
+Relational database is a database that store data in tables which are interelated.
+example: mysql, postgresql, microsoft SQL server, oracle database.
+
+
+State and Explain three classifications of SQL?
+data defination language which is used to define the strcture of the database.
+data manipulation language which is used to manipulate data with operations like to retrieve, update, insert, delete 
+data control language which is used to give privillages to users through the grant and revoke command.
+
+What is the difference between a Primary Key and a Foreign Key?
+A primary key is a filed that is used to identify a record uniquely while a foreign key is a key that is a field in a table that 
+links the table to a primary key in another table.
+
+What is an Entity-Relationship Diagram?
+ERD is a graphical representation of the realtionship among entities in a database.
+
+What are the advantages of relational databases?
+It is easy to query 
+Flexible as they can handle complex queries.
+Reduces data duplication through normalization
+Robust security feature through access control to prevent unauthorized access.
+data integrity by using the primary and foreign keys which ensures accuracy and consistence.
+
+State four types of data type used to store data in tables?
+interger which stores whole numbers
+varchar which stores variable length strings ot text.
+data and time.
+float and decimals
+
+What is the purpose of a database management system (DBMS)?
+ data organization
+ data security
+ data integrity
+ data recovery
+ concurrency control
+
+
+
+
